29b3325d8b0c70a68b4deb0f92f6c1659934f283
erichlof <erichlof@swbell.net>
2014-06-19 00:48:16 -0500

    Update AsteroidPatrolVariables.js

--- js/AsteroidPatrolVariables.js
+++ js/AsteroidPatrolVariables.js
@@ -48,7 +48,7 @@ scene.add(planet);
 
 // SkyBox
 var imagePrefix = "images/skybox/nebula-";
-var directions  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
+var directions  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
 var imageSuffix = ".jpg";
 var skyGeometry = new THREE.BoxGeometry( 2900, 2900, 2900 );	
 
