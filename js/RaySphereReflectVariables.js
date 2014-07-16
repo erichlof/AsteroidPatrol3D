cbd38e27b04dd6d16c805b59d821ae07ea98cf9f
erichlof <erichlof@swbell.net>
2014-07-16 00:54:49 -0500

    Update RaySphereReflectVariables.js

--- js/RaySphereReflectVariables.js
+++ js/RaySphereReflectVariables.js
@@ -122,45 +122,5 @@ var newBulletSpherePos = new THREE.Vector3();
 var collisionNormal = new THREE.Vector3();
 var numberOfCollisions = 0;
 
-THREE.Ray.prototype.intersectSphere = function () {
-
-	// from http://www.scratchapixel.com/lessons/3d-basic-lessons/lesson-7-intersecting-simple-shapes/ray-sphere-intersection/
-		
-	var v1 = new THREE.Vector3();
-		
-	return function ( sphere, optionalTarget ) {
-
-		v1.subVectors( sphere.center, this.origin );
-
-		var tca = v1.dot( this.direction );
-
-		var d2 = v1.dot( v1 ) - tca * tca;
-
-		var radius2 = sphere.radius * sphere.radius;
-			
-		if ( d2 > radius2 ) return null;
-
-		var thc = Math.sqrt( radius2 - d2 );
-			
-		// t0 = first intersect point - entrance on front of sphere
-		var t0 = tca - thc;
-
-		// t1 = second intersect point - exit point on back of sphere
-		var t1 = tca + thc;
-			
-		// test to see if both t0 and t1 are behind the ray - if so, return null
-		if ( t0 < 0 && t1 < 0 ) return null;
-		
-		// test to see if t0 is behind the ray:
-		// if it is, the ray is inside the sphere, so return the second exit point scaled by t1,
-		// in order to always return an intersect point that is in front of the ray.
-		if ( t0 < 0 ) return this.at( t1, optionalTarget );
-
-		// else t0 is in front of the ray, so return the first collision point scaled by t0 
-		return this.at( t0, optionalTarget );
-			
-	};
-	
-}();
 
 // END GLOBAL VARIABLES /////////////////////////////////////////////////////////////////
