1cee311cff6751d1fc5fb0bfa59f12633b27ba94
erichlof <erichlof@swbell.net>
2014-07-16 00:52:21 -0500

    Update RayIntersectSphereVariables.js

--- js/RayIntersectSphereVariables.js
+++ js/RayIntersectSphereVariables.js
@@ -29,11 +29,17 @@ var renderer = new THREE.WebGLRenderer({
 	antialias: true
 });
 
+var light = new THREE.PointLight('rgb(255,255,255)', 1, 0);
+light.position.set(50, 50, -100);
+scene.add(light);
+
 var boxGeometry = new THREE.BoxGeometry(20, 20, 20);
 var boxMaterial = new THREE.MeshLambertMaterial({
 	color: 'rgb(0,255,0)'
 });
 var box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.set(-10,0,-150);
+box.rotation.y = 0.6;
 scene.add(box);
 
 var sphereGeometry = new THREE.SphereGeometry(10);
@@ -41,6 +47,7 @@ var sphereMaterial = new THREE.MeshBasicMaterial({
 	color: 'rgb(255,255,0)'
 });
 var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.copy(light.position);
 scene.add(sphere);
 
 var bulletGeometry = new THREE.SphereGeometry(3);
@@ -50,8 +57,23 @@ var bulletMaterial = new THREE.MeshBasicMaterial({
 var bulletSphere = new THREE.Mesh(bulletGeometry, bulletMaterial);
 scene.add(bulletSphere);
 
-var light = new THREE.PointLight('rgb(255,255,255)', 1, 0);
-scene.add(light);
+var targetGeometry = new THREE.SphereGeometry(3);
+var targetMaterial = new THREE.MeshBasicMaterial({
+	color: 'rgb(0,0,255)'
+});
+var targetSphere = new THREE.Mesh(targetGeometry, targetMaterial);
+scene.add(targetSphere);
+
+var pyramidGeometry = new THREE.CylinderGeometry(0,4,10,4,1);
+var pyramidMaterial = new THREE.MeshNormalMaterial({
+	//color: 'rgb(255,0,0)'
+});
+//pyramidGeometry.applyMatrix( new THREE.Matrix4().makeRotationFromEuler( new THREE.Euler( Math.PI / 2, 0, 0 ) ) );
+pyramidGeometry.applyMatrix( new THREE.Matrix4().makeRotationX( Math.PI / 2) );
+var pyramid = new THREE.Mesh(pyramidGeometry, pyramidMaterial);
+pyramid.position.set(0,5,-30);
+pyramid.lookAt(scene.position);
+scene.add(pyramid);
 
 // FLOOR
 var floorTexture = new THREE.ImageUtils.loadTexture('images/checkerboard.jpg');
@@ -63,7 +85,7 @@ var floorMaterial = new THREE.MeshBasicMaterial({
 });
 var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
 var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-floor.position.y = -10;
+floor.position.y = -100;
 floor.rotation.x = Math.PI / 2;
 scene.add(floor);
 
@@ -87,58 +109,11 @@ var bulletAlive = false;
 //var bulletCounter = 0;
 var canShoot = true;
 
-
 var thrustVector = new THREE.Vector3(0, 0, -1);
 var ship = new THREE.Object3D();
 var shipSpeed = 0;
 var shipVelocity = new THREE.Vector3(0, 0, 0);
 var normalizedShipDirection = new THREE.Vector3(0, 0, 0);
-
-var debugText1 = document.getElementById("debug1");
-var debugText2 = document.getElementById("debug2");
-var debugText3 = document.getElementById("debug3");
-var debugText4 = document.getElementById("debug4");
-
-THREE.Ray.prototype.intersectSphere = function () {
-
-	// from http://www.scratchapixel.com/lessons/3d-basic-lessons/lesson-7-intersecting-simple-shapes/ray-sphere-intersection/
-		
-	var v1 = new THREE.Vector3();
-		
-	return function ( sphere, optionalTarget ) {
-
-		v1.subVectors( sphere.center, this.origin );
-
-		var tca = v1.dot( this.direction );
-
-		var d2 = v1.dot( v1 ) - tca * tca;
-
-		var radius2 = sphere.radius * sphere.radius;
-			
-		if ( d2 > radius2 ) return null;
-
-		var thc = Math.sqrt( radius2 - d2 );
-			
-		// t0 = first intersect point - entrance on front of sphere
-		var t0 = tca - thc;
-
-		// t1 = second intersect point - exit point on back of sphere
-		var t1 = tca + thc;
-			
-		// test to see if both t0 and t1 are behind the ray - if so, return null
-		if ( t0 < 0 && t1 < 0 ) return null;
-		
-		// test to see if t0 is behind the ray:
-		// if it is, the ray is inside the sphere, so return the second exit point scaled by t1,
-		// in order to always return an intersect point that is in front of the ray.
-		if ( t0 < 0 ) return this.at( t1, optionalTarget );
-
-		// else t0 is in front of the ray, so return the first collision point scaled by t0 
-		return this.at( t0, optionalTarget );
-			
-	};
-	
-}();
 
 var collisionSphere = new THREE.Sphere();
 var bulletRay = new THREE.Ray();
@@ -146,5 +121,14 @@ var bulletRayCollisionPoint = new THREE.Vector3();
 var oldBulletSpherePos = new THREE.Vector3();
 var newBulletSpherePos = new THREE.Vector3();
 var collisionNormal = new THREE.Vector3();
+var targetVector = new THREE.Vector3();
+targetVector.subVectors(box.position, targetSphere.position).normalize();
+var lerpAmount = 0.0;
+
+var debugText1 = document.getElementById("debug1");
+var debugText2 = document.getElementById("debug2");
+var debugText3 = document.getElementById("debug3");
+var debugText4 = document.getElementById("debug4");
+
 
 // END GLOBAL VARIABLES /////////////////////////////////////////////////////////////////
