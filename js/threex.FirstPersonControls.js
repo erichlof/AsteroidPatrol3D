54e4d686824271bb790d3c89a556fb01b74dc537
erichlof <erichlof@swbell.net>
2015-01-19 23:56:04 -0600

    Implemented better 3D camera controls (PC and mobile)

--- js/threex.FirstPersonControls.js
+++ js/threex.FirstPersonControls.js
@@ -1,104 +1,77 @@
 /**
  * @author mrdoob / http://mrdoob.com/
- * @author alteredq / http://alteredqualia.com/
- * @author paulirish / http://paulirish.com/
- * adapted for THREEx by erichlof
+ * adapted for virtualButtonJoystick.js by erichlof
+ * https://github.com/erichlof
  */
 
-/** @namespace */
-var THREEx = THREEx || {};
+THREEx.FirstPersonControls = function ( camera ) {
 
-THREEx.FirstPersonControls = function ( object, domElement ) {
+	camera.rotation.set( 0, 0, 0 );
 
-	this.object = object;
-	this.target = new THREE.Vector3( 0, 0, 0 );
+	this.pitchObject = new THREE.Object3D();
+	this.pitchObject.add( camera );
 
-	this.domElement = ( domElement !== undefined ) ? domElement : document;
+	this.yawObject = new THREE.Object3D();
+	this.yawObject.add( this.pitchObject );
+	
+	var that = this;
 
-	this.movementSpeed = 1.0;
-	this.lookSpeed = 0.005;
+	var movementX = 0;
+	var movementY = 0;
+	
+	var onMouseMove = function ( event ) {
 
-	this.lookVertical = true;
+		if (playerAlive) {
+			movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
+			movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
+		
+			that.yawObject.rotation.y -= movementX * 0.002;
+			that.pitchObject.rotation.x -= movementY * 0.002;
 
-	this.mouseX = 0;
-	this.mouseY = 0;
-
-	this.lon = 0;//left and right rotation
-	this.lat = 0;//up and down rotation
-	this.phi = 0;
-	this.theta = 0;
-
-	this.moveForward = false;
-	this.moveBackward = false;
-	this.moveLeft = false;
-	this.moveRight = false;
-
-	this.viewHalfX = 0;
-	this.viewHalfY = 0;
-	//moved
-	var actualMoveSpeed = 0;
-	var actualLookSpeed = 0;
-	var targetPosition = 0;
-	var position = 0;
-
-	if ( this.domElement !== document ) {
-
-		this.domElement.setAttribute( 'tabindex', -1 );
-
-	}
-
-
-	this.handleResize = function () {
-
-		if ( this.domElement === document ) {
-
-			this.viewHalfX = window.innerWidth / 2;
-			this.viewHalfY = window.innerHeight / 2;
-
-		} else {
-
-			this.viewHalfX = this.domElement.offsetWidth / 2;
-			this.viewHalfY = this.domElement.offsetHeight / 2;
-
+			that.pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, that.pitchObject.rotation.x ) );
 		}
 
 	};
 
-	
-	this.update = function( delta ) {
+	document.addEventListener( 'mousemove', onMouseMove, false );
 
-		actualMoveSpeed = delta * this.movementSpeed;
+	this.enabled = false;
 
-		if ( this.moveForward ) this.object.translateZ( - ( actualMoveSpeed ) );
-		if ( this.moveBackward ) this.object.translateZ( actualMoveSpeed );
+	this.getObject = function () {
 
-		if ( this.moveLeft ) this.object.translateX( - actualMoveSpeed );
-		if ( this.moveRight ) this.object.translateX( actualMoveSpeed );
+		return that.yawObject;
 
-		if ( this.moveUp ) this.object.translateY( actualMoveSpeed );
-		if ( this.moveDown ) this.object.translateY( - actualMoveSpeed );
-
-		//clamp vertical motion at north and south poles
-		if ( this.lat > 85 ) this.lat = 85;
-		if ( this.lat < -85 ) this.lat = -85;
-
-		this.phi = THREE.Math.degToRad( 90 - this.lat );
-		
-		//added '- 90' so that on startup, camera faces down -Z axis
-		this.theta = THREE.Math.degToRad( this.lon - 90 );
-		
-		targetPosition = this.target;
-		position = this.object.position;
-
-		targetPosition.x = position.x + Math.sin( this.phi ) * Math.cos( this.theta );
-		targetPosition.y = position.y + Math.cos( this.phi );
-		targetPosition.z = position.z + Math.sin( this.phi ) * Math.sin( this.theta );
-		
-		this.object.lookAt( targetPosition );
-		
 	};
+	
+	this.getYawObject = function () {
 
+		return that.yawObject;
 
-	this.handleResize();
+	};
+	
+	this.getPitchObject = function () {
+
+		return that.pitchObject;
+
+	};
+	
+	this.getDirection = function() {
+
+		// assumes the camera itself is not rotated
+
+		var direction = new THREE.Vector3( 0, 0, -1 );
+		var rotation = new THREE.Euler( 0, 0, 0, "YXZ" );
+
+		return function( v ) {
+
+			rotation.set( that.pitchObject.rotation.x, that.yawObject.rotation.y, 0 );
+
+			v.copy( direction ).applyEuler( rotation );
+
+			return v;
+
+		};
+
+	}();
 
 };
